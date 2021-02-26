1affbf3de14ef4f739681e0848257ac4a7c89a02
SebastianSafaiRad <60371753+SebastianSafaiRad@users.noreply.github.com>
2021-02-26 16:23:18 -0700

    beggining of assignment 2

---
+++ assign2.py
@@ -0,0 +1,21 @@
+from lists import UnorderedList
+import unittest
+
+class QueueLL:
+    """ ADD DOCUMENTATION HERE, REMOVING/REPLACING COMMENTS IN ALL CAPS """
+
+    def __init__(self):
+        """ Create the one field/attribute needed for our QueueLL class """
+        self.queue = UnorderedList()
+
+    # ADD ALL REQUIRED METHODS HERE - SEE PART 1 DIRECTIONS IN Canvas
+    # (REMOVE/REPLACE THIS AND ANY OTHER COMMENTS IN CAPS)
+
+
+class TestQueueLL(unittest.TestCase):
+    """ ADD DOCUMENTATION HERE, REMOVING/REPLACING COMMENTS IN ALL CAPS """
+
+    # ADD AT LEAST TWO UNIT TESTS HERE TO TEST THAT QUEUELL METHODS WORK AS EXPECTED
+    # (REMOVE/REPLACE THIS AND ANY OTHER COMMENTS IN CAPS)
+
+# SEE PART 2 BELOW FOR THE CONTINUATION OF THIS FILE
