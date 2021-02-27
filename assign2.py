5246d16b9e495853188c1924af356b1bb2b2eb5e
SebastianSafaiRad <60371753+SebastianSafaiRad@users.noreply.github.com>
2021-02-26 17:27:29 -0700

    finished methods of part 1

--- assign2.py
+++ assign2.py
@@ -8,6 +8,25 @@ class QueueLL:
         """ Create the one field/attribute needed for our QueueLL class """
         self.queue = UnorderedList()
 
+    def __str__(self):
+        return self.queue.__str__()
+
+    def isEmpty(self):
+        return self.queue.isEmpty()
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        return self.queue.pop(0)
+
+    def peek(self):
+        return self.queue.head
+
+    def size(self):
+        return self.queue.size()
+
+
     # ADD ALL REQUIRED METHODS HERE - SEE PART 1 DIRECTIONS IN Canvas
     # (REMOVE/REPLACE THIS AND ANY OTHER COMMENTS IN CAPS)
 
